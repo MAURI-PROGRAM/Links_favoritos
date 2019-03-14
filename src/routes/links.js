710ec4b117d60d90c61b669e83c7f0f01c17d411
unknown <merchan.ber.mau@gmail.com>
2019-03-13 19:08:25 -0500

    añadir y visualizar links

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,12 +4,25 @@ const router = express.Router();
 const pool = require('../database');
 
 router.get('/add',(req,res)=>{
-    res.render('links/add')
+    res.render('links/add');
 });
 
-router.post('/add',(req,res)=>{
-    res.send('recivido')
+router.post('/add',async (req,res)=>{
+    const {titulo,url,descripcion} = req.body;
+    const newLink={
+        titulo,
+        url,
+        descripcion
+    }
+    await pool.query('INSERT INTO links set ?',[newLink]);
+    res.redirect('/links');
 });
 
+router.get('/',async (req,res)=>{
+    const links = await pool.query('SELECT * FROM links');
+    console.log(links);
+    res.render('links/list',{links});
+
+});
 
 module.exports=router;
